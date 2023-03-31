845e1acc49842f49f58ad1cf6c023e03d5afbaf4
Hazem179 <43998792+Hazem179@users.noreply.github.com>
2023-03-31 02:46:36 +0200

    Update settings.py

--- QS/settings.py
+++ QS/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'QS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Qs',
-        'USER': 'postgres',
-        'PASSWORD': '251hazem179',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
